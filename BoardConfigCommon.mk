1c0621eae471663ebe4fe523ab23af42daa6d389
mttronc <mrtn.mtth@gmx.de>
2018-08-05 20:49:06 +0200

    Rebrand cmhw to lineagehw
    Change-Id: I7f40b5da3b609a288a30f13cee5f2338e13dda2c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2015 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -130,7 +130,7 @@ VSYNC_EVENT_PHASE_OFFSET_NS := 2500000
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 0000000
 
 # Hardware tunables framework
-BOARD_HARDWARE_CLASS := $(VENDOR_PATH)/cmhw/
+BOARD_HARDWARE_CLASS := $(VENDOR_PATH)/lineagehw/
 
 # GPS
 TARGET_NO_RPC := true
